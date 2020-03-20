2824d474f73bcb8389e96dc8de23fbabfa281144
circlesabound <circlesabound@users.noreply.github.com>
2020-03-20 16:27:17 +1100

    Fix version output

--- src/server/health_handlers.rs
+++ src/server/health_handlers.rs
@@ -9,10 +9,15 @@ pub async fn ping() -> Result<impl Reply, Infallible> {
 pub async fn version() -> Result<impl Reply, Infallible> {
     match env::var("GIT_COMMIT_HASH") {
         Ok(commit_hash) => {
-            Ok(warp::reply::json(&commit_hash))
+            Ok(warp::reply::json(&Version { commit_hash }))
         },
         Err(_) => {
-            Ok(warp::reply::json(&"unversioned"))
+            Ok(warp::reply::json(&Version { commit_hash: "unversioned".into() }))
         }
     }
+}
+
+#[derive(serde::Serialize)]
+struct Version {
+    commit_hash: String
 }
